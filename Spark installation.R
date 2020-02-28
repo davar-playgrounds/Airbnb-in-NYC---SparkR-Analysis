9a94dd9dedf8b68bd7063af20155741a6fd941ba
dominikklepl <dominikklepl@gmail.com>
2020-02-28 13:35:40 +0000

    cleaned code

--- Spark installation.R
+++ Spark installation.R
@@ -1,29 +1,23 @@
+system("sudo apt install openjdk-8-jdk")
 install.packages("sparklyr")
-install.packages("SparkR")
 
 library(sparklyr)
 spark_install(version = "2.2")
 
-# The following two commands remove any previously installed H2O packages for R.
-if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
-if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
-
 # Install packages H2O depends on
-pkgs <- c("methods", "statmod", "stats", "graphics", "RCurl", "jsonlite", "tools", "utils")
+pkgs = c("methods", "statmod", "stats", "graphics", "RCurl", "jsonlite", "tools", "utils")
 for (pkg in pkgs) {
-  if (! (pkg %in% rownames(installed.packages()))) { install.packages(pkg) }
+  install.packages(pkgs)
 }
 
 install.packages("h2o", "3.28.0.3")
 
 install.packages("rsparkling", type = "source", repos = "http://h2o-release.s3.amazonaws.com/sparkling-water/spark-2.2/3.28.0.3-1-2.2/R")
 
+library(sparklyr)
 library(rsparkling)
-
 sc = spark_connect(master = "local", version = "2.2.1")
-
 hc = H2OContext.getOrCreate(sc)
-
 hc$openFlow()
 
 library(dplyr)
